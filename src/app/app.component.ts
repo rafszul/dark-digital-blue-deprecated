6402ad424ceb19e0babcd17bfac306362b509eb2
rafszul <rafszul@gmail.com>
2016-12-25 12:45:01 +0000

    electron

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  const electron = require('electron');
+
+const {app, BrowserWindow, globalShortcut} = electron;
+
+let mainWindow;
+
+app.on('ready', ()=>{
+    mainWindow = new BrowserWindow({
+        width: 1000,
+        height: 700
+    });
+
+    globalShortcut.register('CmdOrCtrl+Shift+d', ()=>{
+        mainWindow.webContents.togglrDevTools();
+    });
+
+    mainWindow.setTitle('ace of base electrified');
+    mainWindow.loadURL('http://aceofbase.com/');
+
+    mainWindow.on('closed', ()=>{
+        mainWindow = null;
+    });
+
+});
+
+app.on('will-quit', ()=>{
+    globalShortcut.unregisterAll();
+});
+
+app.on('window-all-closed', ()=>{
+    app.qiut();
+});
+
 }
